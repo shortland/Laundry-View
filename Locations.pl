e27efd0afb370db730ceff2c455af7b063689498
Ilan Kleiman <ilankleiman@gmail.com>
2017-12-03 22:05:47 -0500

    Session carries based on school/location. Read Description
    If your IP matches a specific location, then it’s not necessary for you
    to supply a location in the URL.
    ?s=…
    
    Though since this script is being run on a server (IP not a campus IP),
    a specific location is necessary and the cookie supplied to the script
    needs to be carried over for every request… So that’s what this push
    accomplishes. Carries over PHPSESS cookie ID so that a location is kept
    consistent.

--- Locations.pl
+++ Locations.pl
@@ -5,24 +5,33 @@ use warnings;
 use HTTP::Request;
 use LWP::UserAgent;
 use Path::Tiny;
-use JSON;
+use JSON qw(encode_json);
+use CGI::Carp qw(fatalsToBrowser);
+use CGI;
 
 sub renewLocationData {
-	my @quads = quadNames();
+	my ($school, $ua) = @_;
+	my @quads = quadNames($ua);
+	if ($quads[0] =~ /^DEMO LOCATION$/) {
+		return "Host not using campus WIFI. Please define a campus.</br>\n";
+	}
+	elsif ($quads[0] =~ /^$/) {
+		return "Error fetching data</br>\n";
+	}
 	my $jsonObj = {quads => []};
 	for (my $i = 0; $i < scalar(@quads); $i++) {
-		$jsonObj = quadBuildings($quads[$i], $jsonObj);
+		$jsonObj = quadBuildings($quads[$i], $jsonObj, $ua);
 	}
-	path("location_data.json")->spew(encode_json($jsonObj));
-	return "location_data.json has been refreshed with new data\n";
+	path("js/location_data.json")->spew(encode_json($jsonObj));
+	chmod 0777, "js/location_data.json" or die "Couldn't chmod js/location_data.json: $!";
+	return "location_data.json has been refreshed with new data</br>\n";
 }
 
 sub quadBuildings {
-	my ($quadName, $jsonObj) = @_;
-	my $baseURL = "http://classic.laundryview.com/lvs.php";
-	my $raw = getData($baseURL);
+	my ($quadName, $jsonObj, $ua) = @_;
+	my $baseURL = "http://laundryview.com/lvs.php?s=2376";
+	my $raw = getData($baseURL, $ua);
 	my @splitChunks = split(/<div class="h4" style="cursor:hand;cursor:pointer;" onclick="/m, $raw);
-
 	my $i = 0;
 	for my $chunk (@splitChunks) {
 		$chunk =~ m/uptween\(['a-z0-9,]+\);">\s*(\w+-*\s*\w+)/g;
@@ -35,7 +44,7 @@ sub quadBuildings {
 				}
 				my @buildingCodes = ($splitChunks[$i] =~ m/<a href="laundry_room.php\?lr=(\d+)/g);
 				my $obj = {name => $found, buildings => [@buildingNames], ids => [@buildingCodes]};
-				push(@{$jsonObj->{quads}}, $obj); 
+				push(@{$jsonObj->{quads}}, $obj);
 				last;
 			}
 		}
@@ -45,20 +54,57 @@ sub quadBuildings {
 }
 
 sub quadNames {
-	my $baseURL = "http://classic.laundryview.com/lvs.php";
-	my $raw = getData($baseURL);
+	my ($ua) = @_;
+	my $baseURL = "http://laundryview.com/lvs.php?s=2376";
+	my $raw = getData($baseURL, $ua);
 	my @names = ($raw =~ m/<div class=\"h4\" style=\"cursor:hand;cursor:pointer;\" onclick="[\S]+">[\s]+([\S]+\s?[\S]+)/g);
 	return @names;
 }
 
-sub getData {
-	my ($url) = @_;
-	my $request = HTTP::Request->new(GET => $url);
+sub setSession {
+	my ($school) = @_;
+	my $request = HTTP::Request->new(GET => "http://laundryview.com/" . $school);
 	my $ua = LWP::UserAgent->new;
-	my $response = $ua->request($request)->content();
+	$ua->cookie_jar({});
+	$ua->request($request);
+	return $ua;
+}
+
+sub getData {
+	my ($url, $ua) = @_;
+	my $request = HTTP::Request->new(GET => $url);
+	my $response = $ua->request($request)->content;
 	return $response;
 }
 
 BEGIN {
-	print renewLocationData();
+	my $cgi = new CGI;
+	print $cgi->header(-type => "text/html");
+
+	my $defaultSchool = "stonybrook";
+
+	if (defined $cgi->param("s")) {
+		$defaultSchool = $cgi->param("s");
+	}
+
+	my $capitalizeSchool = uc(substr($defaultSchool, 0, 1)) . substr($defaultSchool, 1);
+	print my $html = 
+qq{<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>$capitalizeSchool Alerts</title>
+	<link rel="manifest" href="js/manifest.json">
+	<meta name="viewport" content="user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width, height=device-height, target-densitydpi=device-dpi" />
+	<script type="text/javascript" src='js/jquery.js'></script>
+	<script type="text/javascript" src='js/jquery_colors.js'></script>
+	<script type="text/javascript" src='js/script.js'></script>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<center>
+		<h3>School: <i>$capitalizeSchool</i></h3>
+	</center>
+</body>
+</html>};
+	print renewLocationData($defaultSchool, setSession($defaultSchool));
 }
